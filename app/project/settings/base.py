23bc26758bf7361c8379ce1805c32a12f3c9cb1b
Alexey Kuzyashin <terr.kuzyashin@gmail.com>
2020-06-16 00:15:14 +0300

    swagger

--- app/project/settings/base.py
+++ app/project/settings/base.py
@@ -46,6 +46,9 @@ CHANNEL_LAYERS = {
     },
 }
 
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 SWAGGER_SETTINGS = {
     'DEFAULT_API_URL': 'https://backend.mobile.fitnesskit.awkr.site/'
 }
