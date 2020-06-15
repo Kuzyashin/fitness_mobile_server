27d36facec18a94d183f35adf10a8c618e404614
Alexey Kuzyashin <terr.kuzyashin@gmail.com>
2020-06-15 15:40:06 +0300

    fix

--- app/project/settings/base.py
+++ app/project/settings/base.py
@@ -30,8 +30,8 @@ sys.path.append(os.path.join(PROJECT_ROOT, 'applications'))
 
 ROOT_URLCONF = 'project.urls'
 
-WSGI_APPLICATION = 'project.wsgi.application'
-ASGI_APPLICATION = "project.asgi.application"
+WSGI_APPLICATION = 'wsgi.application'
+ASGI_APPLICATION = "routing.application"
 
 REDIS_HOST = os.environ['REDIS_HOST']
 
