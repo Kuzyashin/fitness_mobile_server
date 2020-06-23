93741e91f2ce9dd9175f664399fb351717e62b4a
AKuzyashin <terr.kuzyashin@gmail.com>
2020-06-23 20:27:34 +0300

    New style

--- app/project/applications/core/base.py
+++ app/project/applications/core/base.py
@@ -97,9 +97,7 @@ class DjangoViewAsConsumer(BaseConsumer):
     def get_server_name_port(self):
         server_host = self.scope.get('server')[0]
         server_port = self.scope.get('server')[1]
-        print(dict(self.scope.get('headers')))
-        print(dict(self.scope.get('headers').decode()))
-        server_name = dict(self.scope.get('headers')).get('host')
+        server_name = dict(self.scope.get('headers')).get(b'host')
         if server_name:
             return server_name, server_port
         else:
@@ -112,7 +110,7 @@ class DjangoViewAsConsumer(BaseConsumer):
         request.session = self.scope.get("session", None)
         request.query_params = self.get_querydict(request)
 
-        request.META['HTTP_X_FORWARDED_HOST'] = dict(self.scope.get('headers')).get('host')
+        request.META['HTTP_X_FORWARDED_HOST'] = dict(self.scope.get('headers')).get(b'host')
         request.META['SERVER_NAME'], request.META['SERVER_PORT'] = self.get_server_name_port()
         request.META["HTTP_CONTENT_TYPE"] = "application/json"
         request.META["HTTP_ACCEPT"] = "application/json"
